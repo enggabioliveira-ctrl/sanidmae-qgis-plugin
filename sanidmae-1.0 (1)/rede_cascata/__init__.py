8f83dd2fa7d3805819a1d52b501bc4b78dfe9b7e
enggabioliveira-ctrl <eng.gabioliveira@gmail.com>
2026-08-17 16:30:22 -0300

    Delete sanidmae-1.0 (1)/rede_cascata directory

--- sanidmae-1.0 (1)/rede_cascata/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Ponto de entrada do plugin. O QGIS chama classFactory() ao carregar o plugin.
-"""
-
-
-def classFactory(iface):
-    from .rede_cascata import RedeCascataPlugin
-    return RedeCascataPlugin(iface)
